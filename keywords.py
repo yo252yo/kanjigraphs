9a84715bf977b988802b804071b7deae43b88dd8
yoann@9s <yo252yo@gmail.com>
2022-06-26 16:18:36 +0200

    print kw at the end in hope that makes them more often on top

--- keywords.py
+++ keywords.py
@@ -29,23 +29,6 @@ class Keywords(object):
                     if not l[0] in keywords:
                         keywords[l[0]] = kw
 
-
-        for kw in keywords:
-            label = ("<b>" + kw + "</b>\n " + str(keywords[kw]))#.decode('utf-8')
-
-            ease = 0
-            d = 0
-            for kanji in kw:
-                if kanji in data.ease:
-                    ease += data.ease[kanji]
-                    d += 1
-            if d>0:
-                ease /= d
-
-            fe = str((0.2 + 0.8 * ease))
-            fcolor = "0.4 " + fe + " 1"
-            node = dot.node(Keywords.labelof(keywords, kw), fontsize='50', label=kw, shape="rectangle", style='filled', fillcolor=fcolor)
-
         newkanjis = set()
         for kanji in kanjis:
             for similar in data.similars[kanji]:
@@ -76,5 +59,20 @@ class Keywords(object):
         for kw in keywords:
             if len(kw) > 1 and kw[0] in data.descriptions and kw[1] in data.descriptions:
                 dot.edge(data.descriptions[kw[0]], data.descriptions[kw[1]], color="green", len="0.5")#.decode('utf-8')
+
+            label = ("<b>" + kw + "</b>\n " + str(keywords[kw]))#.decode('utf-8')
+
+            ease = 0
+            d = 0
+            for kanji in kw:
+                if kanji in data.ease:
+                    ease += data.ease[kanji]
+                    d += 1
+            if d>0:
+                ease /= d
+
+            fe = str((0.2 + 0.8 * ease))
+            fcolor = "0.4 " + fe + " 1"
+            node = dot.node(Keywords.labelof(keywords, kw), fontsize='50', label=kw, shape="rectangle", style='filled', fillcolor=fcolor)
 
         return dot
